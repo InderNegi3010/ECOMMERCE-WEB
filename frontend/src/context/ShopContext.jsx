a9aff405726b325a72005c13fba2390246473106
InderNegi3010 <snegi1070@gmail.com>
2025-07-17 12:06:05 +0530

    removing the unnecessary line from the shopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets";
 import Toastify from "toastify-js";
 import "toastify-js/src/toastify.css";
 import { useNavigate } from "react-router-dom";
